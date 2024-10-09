6b08e32d0b78e30903b62532d6e6fda8a9bcec6e
Виктор Корнеплод <yarmisikov@gmail.com>
2024-10-10 01:42:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0710
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. Написать программный код, который реализует диалог с пользователем по образцу консоли: 
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} = {a+b}\n{a} * {b} = {a*b}");
+            //Задание 1.2
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} = {b} + {a}\n{a} * {b} = {b} * {a}");
+            //Задание 1.3
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a} + {b} + {c}= {a + b + c}");
+            //Задание 1.4
+            //Console.Write("a = ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b = ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{a:f1}*{b:f1}={a*b:f1}");
+            //Задание 1.5
+            //Console.Write("a = ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write($"{a:f3}/{b:f3}={a / b:f3}");
+            //Задание 1.6
+            //Console.WriteLine("a=");
+            //double a= double.Parse(Console.ReadLine());
+            //Console.WriteLine("b=");
+            //double b= double.Parse(Console.ReadLine());
+            //Console.WriteLine("c=");
+            //double c= double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f2}+{b:f2})+{c:f2} = {c:f2} + ({b:f2}+ {a:f2})");
+            //Задача 1.7
+            //Console.WriteLine("a=");
+            //int a=int.Parse(Console.ReadLine());
+            //Console.WriteLine("b=");
+            //int b=int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a+b={a}+{b}={a+b}\t a-b={a}-{b}={a-b}\t a*b={a}*{b}={a*b}");
+            //Задача 1.8
+            //Console.Write("Номинал купюры = ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Кол-во купюры = ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write($"Сумма денег = {b*a:f2}");
+            //Задание 2.1 Написать программный код, который реализует вычислительную задачу: 
+            //Console.Write("R=");
+            //double R = double.Parse(Console.ReadLine());
+            //Console.Write($"Длина = {2*Math.PI*R}\n Площадь = {Math.PI * Math.Pow (R,2)}");
+            //Задание 2.2
+            //Console.WriteLine("Кол-во Жителей =");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("S =");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Плоность = {a / b:f2}");
+            //Задание 2.3
+            //Console.Write("a = ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b = ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"P = {Math.Sqrt(Math.Pow(a, 2)) + (Math.Pow(b, 2)) + a + b}");
+            //Задание 2.4
+            //Console.Write("a = ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"P = {a+a+b+b}\nC = {Math.Sqrt(Math.Pow(a,2))+ (Math.Pow(b,2))}");
+            //Задание 2.5
+            //Console.Write("x1 = ");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.Write("y1 = ");
+            //double y1 = double.Parse(Console.ReadLine());
+            //Console.Write("x2 = ");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.Write("y2 = ");
+            //double y2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{Math.Sqrt(Math.Pow(x2,2)-Math.Pow(x1,1)) + (Math.Pow(y2,2) - Math.Pow(y1,2))}");
+            //Задание 2.6
+            //Console.WriteLine("Введите стоимость 1 кг конфет:");
+            //double priceCandy = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите стоимость 1 кг печенья:");
+            //double priceCookies = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите стоимость 1 кг яблок:");
+            //double priceApples = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите количество кг конфет:");
+            //double weightCandy = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите количество кг печенья:");
+            //double weightCookies = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите количество кг яблок:");
+            //double weightApples = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Общая стоимость покупки: {(priceCandy * weightCandy) + (priceCookies * weightCookies) + (priceApples * weightApples)} руб.");
+            //Задание 2.7
+            //Console.WriteLine("Введите двузначное число:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if (number < 10 || number > 99)
+            //{
+            //Console.WriteLine("Ошибка: введите двузначное число.");
+            //}
+            //else
+            //{
+            //int tens = number / 10;
+            //int units = number % 10;
+            //int sum = tens + units;
+            //int product = tens * units;
+            //Console.WriteLine($"Число десятков: {tens}\nЧисло единиц: {units}\nСумма цифр: {sum}\nПроизведение цифр: {product}");
+            Console.ReadLine();
+            }
+        }
+    }
+}
